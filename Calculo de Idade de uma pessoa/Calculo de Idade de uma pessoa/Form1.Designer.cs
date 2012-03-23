0c431fef50fd9d0fce672e7677f03118a3deeb8b
DouglasMontezano <douglasmontezano@hotmail.com>
2012-03-23 20:53:53 -0300

    segundo comitt

---
+++ Calculo de Idade de uma pessoa/Calculo de Idade de uma pessoa/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Calculo_de_Idade_de_uma_pessoa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.lblDiasdevida = new System.Windows.Forms.Label();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblHoras = new System.Windows.Forms.Label();
+            this.lblHorasdevida = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblDiasvida = new System.Windows.Forms.Label();
+            this.lblHorasvida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(12, 30);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(55, 15);
+            this.lblIdade.TabIndex = 0;
+            this.lblIdade.Text = " Idade :\r\n";
+            this.lblIdade.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(87, 30);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(41, 20);
+            this.txtIdade.TabIndex = 1;
+            // 
+            // lblDiasdevida
+            // 
+            this.lblDiasdevida.AutoSize = true;
+            this.lblDiasdevida.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiasdevida.Location = new System.Drawing.Point(108, 75);
+            this.lblDiasdevida.Name = "lblDiasdevida";
+            this.lblDiasdevida.Size = new System.Drawing.Size(0, 15);
+            this.lblDiasdevida.TabIndex = 2;
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDias.Location = new System.Drawing.Point(18, 65);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(90, 15);
+            this.lblDias.TabIndex = 3;
+            this.lblDias.Text = "Dias de vida:";
+            this.lblDias.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblHoras
+            // 
+            this.lblHoras.AutoSize = true;
+            this.lblHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoras.Location = new System.Drawing.Point(18, 90);
+            this.lblHoras.Name = "lblHoras";
+            this.lblHoras.Size = new System.Drawing.Size(99, 15);
+            this.lblHoras.TabIndex = 4;
+            this.lblHoras.Text = "Horas de vida:";
+            // 
+            // lblHorasdevida
+            // 
+            this.lblHorasdevida.AutoSize = true;
+            this.lblHorasdevida.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHorasdevida.Location = new System.Drawing.Point(108, 103);
+            this.lblHorasdevida.Name = "lblHorasdevida";
+            this.lblHorasdevida.Size = new System.Drawing.Size(0, 15);
+            this.lblHorasdevida.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.Location = new System.Drawing.Point(354, 200);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 34);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblDiasvida
+            // 
+            this.lblDiasvida.AutoSize = true;
+            this.lblDiasvida.Location = new System.Drawing.Point(114, 67);
+            this.lblDiasvida.Name = "lblDiasvida";
+            this.lblDiasvida.Size = new System.Drawing.Size(25, 13);
+            this.lblDiasvida.TabIndex = 7;
+            this.lblDiasvida.Text = "------";
+            this.lblDiasvida.Click += new System.EventHandler(this.lblDiasvida_Click);
+            // 
+            // lblHorasvida
+            // 
+            this.lblHorasvida.AutoSize = true;
+            this.lblHorasvida.Location = new System.Drawing.Point(123, 92);
+            this.lblHorasvida.Name = "lblHorasvida";
+            this.lblHorasvida.Size = new System.Drawing.Size(22, 13);
+            this.lblHorasvida.TabIndex = 8;
+            this.lblHorasvida.Text = "-----";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(471, 262);
+            this.Controls.Add(this.lblHorasvida);
+            this.Controls.Add(this.lblDiasvida);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblHorasdevida);
+            this.Controls.Add(this.lblHoras);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.lblDiasdevida);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.lblIdade);
+            this.Name = "Form1";
+            this.Text = "Dias e Horas de vida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label lblDiasdevida;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblHoras;
+        private System.Windows.Forms.Label lblHorasdevida;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblDiasvida;
+        private System.Windows.Forms.Label lblHorasvida;
+    }
+}
+
---
+++ Desconto/Desconto/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Desconto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblValordaCompra = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblDesconto = new System.Windows.Forms.Label();
+            this.lblValortotal = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtDesconto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(232, 227);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(87, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblValordaCompra
+            // 
+            this.lblValordaCompra.AutoSize = true;
+            this.lblValordaCompra.Location = new System.Drawing.Point(31, 30);
+            this.lblValordaCompra.Name = "lblValordaCompra";
+            this.lblValordaCompra.Size = new System.Drawing.Size(108, 13);
+            this.lblValordaCompra.TabIndex = 1;
+            this.lblValordaCompra.Text = "Valor da Compra :";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(31, 227);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(44, 13);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Total :";
+            // 
+            // lblDesconto
+            // 
+            this.lblDesconto.AutoSize = true;
+            this.lblDesconto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesconto.Location = new System.Drawing.Point(31, 55);
+            this.lblDesconto.Name = "lblDesconto";
+            this.lblDesconto.Size = new System.Drawing.Size(82, 13);
+            this.lblDesconto.TabIndex = 3;
+            this.lblDesconto.Text = "Desconto % :";
+            // 
+            // lblValortotal
+            // 
+            this.lblValortotal.AutoSize = true;
+            this.lblValortotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValortotal.Location = new System.Drawing.Point(28, 187);
+            this.lblValortotal.Name = "lblValortotal";
+            this.lblValortotal.Size = new System.Drawing.Size(191, 33);
+            this.lblValortotal.TabIndex = 4;
+            this.lblValortotal.Text = "----------------";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(145, 29);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // txtDesconto
+            // 
+            this.txtDesconto.Location = new System.Drawing.Point(145, 55);
+            this.txtDesconto.Name = "txtDesconto";
+            this.txtDesconto.Size = new System.Drawing.Size(100, 20);
+            this.txtDesconto.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 262);
+            this.Controls.Add(this.txtDesconto);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblValortotal);
+            this.Controls.Add(this.lblDesconto);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblValordaCompra);
+            this.Controls.Add(this.btnCalcular);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblValordaCompra;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblDesconto;
+        private System.Windows.Forms.Label lblValortotal;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtDesconto;
+    }
+}
+
